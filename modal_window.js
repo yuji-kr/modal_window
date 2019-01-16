e9a4ef7d703bbbd299b9817e3595ea6cda6a124b
Yuji KUROSU <46700941+yuji-kr@users.noreply.github.com>
2019-01-16 17:52:55 +0900

    Add files via upload

--- modal_window.js
+++ modal_window.js
@@ -1,29 +1,18 @@
-$(function(){
-	
-	$(".modalOpen").click(function(){
-		
-		var navClass = $(this).attr("class"),
-            href = $(this).attr("href");
-			
-        if(href === "#modal02") {
-        $(href).children(".inner").css("animation","modal 0.5s forwards");
-        }
-        $(href).fadeIn();
-		$(this).addClass("open");
-		return false;
-	});
-	
-	$(".modalClose").click(function(){
-      
-      var parentsID = $(this).parents(".modal").attr("id");
-      
-      if(parentsID === "modal02") {
-        $(this).parents(".modal").children(".inner").css("animation","modalClose 0.5s forwards");
-      }
-      
-      $(this).parents(".modal").fadeOut();
-      $(".modalOpen").removeClass("open");
-      return false;
-	});  
+$(function(){  
+    var a = $('.container'),
+        b = $('.overlay'),
+        c = $('.open'),
+        d = $('.close'); 
+  
+    c.on('click',function(){
+      a.fadeIn();
+    });
     
-});
+    d.on('click',function(){
+      a.fadeOut();
+    });
+    
+    b.on('click',function(){
+      a.fadeOut();
+    });
+  });
